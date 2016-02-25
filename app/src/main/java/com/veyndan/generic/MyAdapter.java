202db9be52270d933d67d19b6ccd5debed1e883e
veyndan <veyndan89@gmail.com>
2016-02-25 20:01:00 +0000

    Add line spacings

--- app/src/main/java/com/veyndan/generic/MyAdapter.java
+++ app/src/main/java/com/veyndan/generic/MyAdapter.java
@@ -2,24 +2,31 @@ package com.veyndan.generic;
 
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
+import android.support.v7.widget.AppCompatImageButton;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     private String[] dataset;
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView name;
+        final TextView about;
+        final TextView description;
         final ImageView profile;
         final ImageView image;
-        final ImageButton heart;
+        final AppCompatImageButton heart;
 
         public ViewHolder(View v) {
             super(v);
-            heart = (ImageButton) v.findViewById(R.id.heart);
+            name = (TextView) v.findViewById(R.id.name);
+            about = (TextView) v.findViewById(R.id.about);
+            description = (TextView) v.findViewById(R.id.description);
+            heart = (AppCompatImageButton) v.findViewById(R.id.heart);
             profile = (ImageView) v.findViewById(R.id.profile);
             image = (ImageView) v.findViewById(R.id.image);
 
